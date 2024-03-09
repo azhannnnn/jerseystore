cfcbe9d9287a5f36595c1adf37a514e2cbd1d882
Azhan Khan <azhanborntowin22@gmail.com>
2024-03-09 15:19:01 +0530

    final

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = BASE_DIR/"media"
 MEDIA_URL ="/media/"
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 KEY = 'rzp_test_AcIEh6rX45zRp8'
 SECRET = 'alxj2MIEOtVrhPpGGbMyFvmX'
