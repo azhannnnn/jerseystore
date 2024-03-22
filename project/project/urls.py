ea0949b8ee9b24c302e8b1cf3cce6fa61444293b
Azhan Khan <azhanborntowin22@gmail.com>
2024-03-22 15:29:59 +0530

    update

--- project/project/urls.py
+++ project/project/urls.py
@@ -17,10 +17,35 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
+from app.views import *
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    
+    path('',home,name='home'),
+    path('products/',products,name='products'),
+    path('productdetail/<int:pk>/',productdetail,name='productdetail'),
+    path('add_cart/<int:pk>', add_cart, name="add_cart"),
+    path('cart/',cart,name='cart'),
+    path('remove/<int:pk>', remove, name="remove"),
+    path('account/',account,name='account'),
+    path('checkout/',checkout,name='checkout'),
+    path('user/',user,name='user'),
+    
+    path('registerpage/',registerpage,name='registerpage'),
+    path('loginpage/',loginpage,name='loginpage'),
+    path('register/',register,name='register'),
+    path('login/',login,name='login'),
+    path('logout/',logout,name='logout'),
+
+    path('payment/',payment,name='payment'),
+    path('success/',success,name='success'),
+    path('myorder/',myorder,name='myorder'),
+
+    #============================================================================================#
+
+   
+    
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
